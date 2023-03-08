061a6e9ffd3e10394f23e231060eb7947708b0b5
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2023-03-08 15:50:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationview.fragments.FavouritesFragment
+import com.example.bottomnavigationview.fragments.HomeFragment
+import com.example.bottomnavigationview.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, HomeFragment())
+            .commit()
+
+        bottomNavigationView.setOnItemSelectedListener {
+            var fragment: Fragment? = null
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    fragment = HomeFragment()
+                }
+                R.id.nav_favourites -> {
+                    fragment = FavouritesFragment()
+                }
+                R.id.nav_search -> {
+                    fragment = SearchFragment()
+                }
+            }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment!!)
+                .commit()
+
+            return@setOnItemSelectedListener true
+        }
+    }
+}
